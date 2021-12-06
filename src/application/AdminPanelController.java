bd26d2cc405c2b8ccaef23723e5a6b93b1ccdb59
Anas Hameed <hameed.anas@hotmail.com>
2021-12-06 21:57:44 +0500

    Feat: implement the front end part of CMS

---
+++ src/application/AdminPanelController.java
@@ -0,0 +1,143 @@
+package application;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class AdminPanelController {
+
+    @FXML
+    private Button Logout;
+
+    @FXML
+    private Button Projects;
+
+    @FXML
+    private Button Home;
+
+    @FXML
+    private TableColumn<String, String> Projecttable;
+    
+    @FXML
+    private ComboBox<String> combbox;
+    
+    @FXML
+    private Text ProjectName;
+
+    @FXML
+    private TextField TaskName;
+
+    @FXML
+    private DatePicker TaskStartDate;
+
+    @FXML
+    private Button Addresource;
+
+    @FXML
+    private DatePicker TaskEndDate;
+
+    @FXML
+    private ComboBox<String> combbox1;
+
+    @FXML
+    private TextField taskDetails;
+
+    @FXML
+    private Button addTask;
+    
+    String bgcolor="-fx-background-color: #00008c;";
+    String RemoveBg= "-fx-background-color: none;";
+    
+    void loadScene(ActionEvent event, String file) throws Exception{
+    	AnchorPane root;
+		root = (AnchorPane)FXMLLoader.load(getClass().getResource(file));
+		Scene scene = new Scene(root,600,500);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    	
+    }
+    
+    void loadborderScene(ActionEvent event, String file) throws Exception{
+    	BorderPane root;
+		root = (BorderPane)FXMLLoader.load(getClass().getResource(file));
+		Scene scene = new Scene(root,600,500);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    }
+    
+    @FXML
+    void initialize() throws Exception{
+    	ObservableList<String> list= FXCollections.observableArrayList("Project1", "Project2", "Project3", "Project4");
+    	if(combbox==null) {
+			System.out.println("Hi i am empty combobox");
+			return;
+		}
+    	combbox.setItems(list);
+    	combbox.getSelectionModel().select(0);
+    	Platform.runLater(new Runnable() {
+            public void run() {
+                final Node scrollBar = combbox.lookup(".scroll-bar:vertical");
+                scrollBar.setVisible(false);
+            }
+        });
+    }
+    
+
+    @FXML
+    void loadHome(ActionEvent event) throws Exception {
+    	Home.setStyle(bgcolor);
+    	combbox.setStyle(RemoveBg);
+    	loadScene(event, "ManagerPanelPage.fxml");
+    }
+
+    @FXML
+    void loadlogoutAction(ActionEvent event) throws Exception {
+    	combbox.setStyle(RemoveBg);
+    	Home.setStyle(RemoveBg);
+    	Logout.setStyle(bgcolor);
+    	loadborderScene(event, "SignInPage.fxml");
+    }
+    
+    
+    @FXML
+    void ShowMenuitem(ActionEvent event) throws Exception {
+    	System.out.println("Hello World");
+    	Home.setStyle("-fx-background-color: none;");
+    	combbox.setStyle(bgcolor);
+    	loadScene(event, "ProjectPages.fxml");
+    	
+    }
+    @FXML
+    void addTaskAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ShowResources(ActionEvent event) {
+
+    }
+
+    @FXML
+    void AddResourceAction(ActionEvent event) {
+
+    } 
+}
+
--- src/application/FxController.java
+++ src/application/FxController.java
@@ -1,14 +1,21 @@
 package application;
 
 import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -32,9 +39,38 @@ public class FxController {
     @FXML
     private Button SignInPage;
     
+    @FXML
+    private ComboBox<String> combbox;
+
+    @FXML
+    private TableView<?> table;
+    
+    @FXML
+    private TableColumn<?, ?> Projecttable;
+    
+
+    
+
+  
+    
     private void loadBorderPaneScene(ActionEvent event, String file) {
     	try { 
         	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource(file));
+    		Scene scene = new Scene(root,600,500);
+    		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+    		Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+    	}
+    	catch(Exception err) {
+    		System.out.println(err);
+    		System.out.println("_________________________________________________");
+		}
+    }
+    
+    private void loadAncherPaneScene(ActionEvent event, String file) {
+    	try { 
+        	AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource(file));
     		Scene scene = new Scene(root,600,500);
     		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
     		Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -61,11 +97,21 @@ public class FxController {
     
     @FXML
     void LoadSignUp(ActionEvent event) {
-
+    	
+    	
     }
     @FXML
     void LoadSigInScreen(ActionEvent event) {
+    	// authentication for the user required Here
+    
+    	loadAncherPaneScene(event, "ManagerPanelPage.fxml");
+    }
+    
+    @FXML
+    void ShowMenuitem(ActionEvent event) {
+    	System.out.println("Hello");
 
     }
 
+    
 }
